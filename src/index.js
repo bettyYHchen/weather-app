74c1dbb20527657fa3187090e1a76133e4ee9649
bettyYHchen <betty_c@outlook.com>
2020-11-20 06:45:27 +0800

    Updates
    done week2 and week3 homework

---
+++ src/index.js
@@ -0,0 +1,39 @@
+let weather = {
+  "paris": {
+    temp: 19.7,
+    humidity: 80
+  },
+  "tokyo": {
+    temp: 17.3,
+    humidity: 50
+  },
+  "lisbon": {
+    temp: 30.2,
+    humidity: 20
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100
+  },
+  "moscow": {
+    temp: -5,
+    humidity: 20
+  }
+};
+
+function capitalizeFirstLetter(str) {
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+let city = prompt("Enter a city").trim().toLowerCase();
+let cityDataExist = false;
+for (let prop in weather) { 
+    if (city === prop) { 
+        cityDataExist = true;
+        alert(`It is currently ${weather[prop].temp}°C (${((weather[prop].temp * 9) / 5) + 32}°F) in ${capitalizeFirstLetter(city)} with a humidity of ${weather[prop].humidity}%.`);
+    }
+}
+
+if (!cityDataExist) { 
+    alert(`Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`);
+}
